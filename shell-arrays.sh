01463b52a2412060f300e45d18bde381f6b2d2db
David Naber <kontakt@dnaber.de>
2016-03-13 16:53:21 +0100

    Add nummeric arrays

--- shell-arrays.sh
+++ shell-arrays.sh
@@ -2,6 +2,21 @@
 
 set -u
 
+## Nummeric array
+LIST=(
+	"foo"
+	"bar"
+	"bazz"
+)
+
+## access
+echo ${LIST[*]} # foo bar bazz
+echo ${LIST[1]} # bar
+
+LIST[0]='bar' # replaces 'foo' with 'bar'
+unset LIST[2] # unsets 'bazz'
+
+
 ## declare an associative array
 declare -A map
 map[foo]="Hello World"
