30ebaf9ab007e1522e11f6fd0616793bab5d86c8
David Naber <kontakt@dnaber.de>
2020-05-10 08:49:22 +0200

    Add multisite.sh

---
+++ multisite.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Wrapper for WP-CLI commands that should apply to all sites in a multisite
+
+set -u
+
+for URL in $(wp site list --field=url); do 
+	echo "Running command for $URL"
+	wp "$@" --url="$URL" 
+done
